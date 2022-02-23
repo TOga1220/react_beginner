c2f7cd02c7e2dc51c71b960fbacfd67afc206061
ogawa <birdchild84@gmail.com>
2022-02-23 18:02:57 +0900

    create header

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,8 +1,27 @@
-export default function TailwindcssTestPage(){
-    return (
-      <div>
-        <h1 className='text-xl text-red-600 p-10'>Tailwind CSS</h1>
-        <h2 className='text-lg text-blue-600'>テストページ</h2>
+const Home = () => {
+  return (
+    <header>
+      <div className="flex justify-between items-center">
+        <a href="">
+          <img src="/logo.png" alt="ロゴ" width="240" height="120" />
+        </a>
+        <ul className="flex text-text-green">
+          <li className="mr-4">
+            <a href="#about">ABOUT</a>
+          </li>
+          <li className="mr-4">
+            <a href="#skills">SKILLS</a>
+          </li>
+          <li className="mr-4">
+            <a href="#values">VALUES</a>
+          </li>
+          <li className="mr-4">
+            <a href="#future">FUTURE</a>
+          </li>
+        </ul>
       </div>
-    );
-  }
+      
+    </header>
+  );
+};
+export default Home;
