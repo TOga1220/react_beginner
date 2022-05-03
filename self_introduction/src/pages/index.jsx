466515d351c7cd3adc2c71338538054f7889cd3d
ogawa <birdchild84@gmail.com>
2022-05-03 18:56:54 +0900

    素材の追加&headerの作成

--- self_introduction/src/pages/index.jsx
+++ self_introduction/src/pages/index.jsx
@@ -1,12 +1,20 @@
 const Home = () => {
     return (
   // returnの中が実際に表示される部分になり、複数行になる場合は一つのタグで囲まれている必要がある。
-      <div>
-        <h1 className="text-2xl">
-          こんにちは！！
-        </h1>
-        <p className="text-xs text-teal-500">お元気ですか？</p>
-      </div>
+    <header className="container">
+        <div className="flex justify-between items-center">
+          <a href="">
+            <img src="./logo.png" alt="ロゴ" width="240" height="120"></img>
+          </a>
+          <ul className="flex text-text-green">
+            <a href="#about" className="mr-4">ABOUT</a>
+          <a href="#skills" className="mr-4">SKILLS</a>
+          <a href="#values" className="mr-4">VALUES</a>
+          <a href="#future" className="mr-4">FUTURE</a>
+          </ul>
+        </div>
+    </header>
+
     );
   };
   export default Home;
