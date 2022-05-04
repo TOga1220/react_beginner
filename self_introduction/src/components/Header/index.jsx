9b7b2600fa573911e49494d0ad70d9029ee78cdc
ogawa <birdchild84@gmail.com>
2022-05-04 23:32:15 +0900

    headerのcomponent化

---
+++ self_introduction/src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+export const Header = () => {
+    return (
+  // returnの中が実際に表示される部分になり、複数行になる場合は一つのタグで囲まれている必要がある。
+    <header className="container">
+        <div className="flex justify-between items-center">
+          <a href="">
+            <img src="./logo.png" alt="ロゴ" width="240" height="120"></img>
+          </a>
+          <ul className="flex text-text-green">
+            <a href="#about" className="mr-4">ABOUT</a>
+            <a href="#skills" className="mr-4">SKILLS</a>
+            <a href="#values" className="mr-4">VALUES</a>
+            <a href="#future" className="mr-4">FUTURE</a>
+          </ul>
+        </div>
+    </header>
+    );
+  };
--- self_introduction/src/pages/index.jsx
+++ self_introduction/src/pages/index.jsx
@@ -1,20 +1,8 @@
+import { Header } from "../components/Header";
+
 const Home = () => {
     return (
-  // returnの中が実際に表示される部分になり、複数行になる場合は一つのタグで囲まれている必要がある。
-    <header className="container">
-        <div className="flex justify-between items-center">
-          <a href="">
-            <img src="./logo.png" alt="ロゴ" width="240" height="120"></img>
-          </a>
-          <ul className="flex text-text-green">
-            <a href="#about" className="mr-4">ABOUT</a>
-          <a href="#skills" className="mr-4">SKILLS</a>
-          <a href="#values" className="mr-4">VALUES</a>
-          <a href="#future" className="mr-4">FUTURE</a>
-          </ul>
-        </div>
-    </header>
-
+      <Header />
     );
   };
   export default Home;
